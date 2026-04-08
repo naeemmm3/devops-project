c380ae9b01622cc7d15da3745a54e5a41efbac99
naeem <naeem@blocksone.tech>
2026-04-08 17:23:42 +0530

    docker final test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 CI/CD is fully working by Naeem!");
+  res.send("🐳 Docker CI/CD is working by Naeem!");
 });
 
 app.listen(3000, () => {
